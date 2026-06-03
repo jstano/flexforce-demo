47a24fffe9820617c8c8224e720df0b2583b71b7
Jeff Stano <jeff@stano.com>
2026-06-03 09:10:56 -0400

    updated

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2592757934" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1488022962" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
